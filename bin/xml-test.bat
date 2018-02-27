0ab70182194b9520335258ed0d4e32aeb301074f
Karel Foršt <karel.forst@anritsu.com>
2018-02-27 09:46:07 +0100

    Small changes insert to RN: all baseline view od RN platform,3rd,..

--- bin/xml-test.bat
+++
@@ -1,3 +0,0 @@
-doc-generator.bat Vzor_changes.xml x.xml _rn-test.doc RN.doc
-doc-generator.bat Vzor_changes.xml x.xml _str-test.doc STR.doc
-doc-generator.bat Vzor_changes.xml x.xml _mail-test.doc MAIL.doc
