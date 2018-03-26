122a2c72f5110addadea5665b4b1e7471dcdb8fa
Karel Foršt <karel.forst@anritsu.com>
2018-03-26 14:43:17 +0200

    malé úpravy xml a rn,

--- bin/rn-test.bat
+++ bin/rn-test.bat
@@ -1,3 +1,7 @@
-doc-generator.bat Vzor_changes.xml x.xml _rn-test.doc RN.doc
-rem doc-generator.bat Vzor_changes.xml x.xml _str-test.doc STR.doc
-rem doc-generator.bat Vzor_changes.xml x.xml _mail-test.doc MAIL.doc
+rem doc-generator.bat Vzor_changes.xml x.xml _rn-test.doc RN.doc
+rem  old doc-generator.bat Vzor_changes.xml x.xml _str-test.doc STR.doc
+rem old doc-generator.bat Vzor_changes.xml x.xml _mail-test.doc MAIL.doc
+
+
+rem ** nova sablona **
+doc-generator.bat Vzor_changes.xml x.xml rn-template-qmpa5.doc qmpa5-.doc
