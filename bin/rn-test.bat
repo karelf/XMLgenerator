05664c50270bd31ee3bd3e042f256353fa5102b1
Karel Foršt <karel.forst@anritsu.com>
2018-04-16 10:38:57 +0200

    přidány soubory pro balíky. odstraněny staré soubory, přesunuty na Anrtitsu backup 300

--- bin/rn-test.bat
+++
@@ -1,7 +0,0 @@
-rem doc-generator.bat Vzor_changes.xml x.xml _rn-test.doc RN.doc
-rem  old doc-generator.bat Vzor_changes.xml x.xml _str-test.doc STR.doc
-rem old doc-generator.bat Vzor_changes.xml x.xml _mail-test.doc MAIL.doc
-
-
-rem ** nova sablona **
-doc-generator.bat Vzor_changes.xml x.xml rn-template-qmpa5.doc qmpa5-.doc
