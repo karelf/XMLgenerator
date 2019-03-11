a987df896ecd42a31d80f6994f997a52e50293f3
Karel Foršt <karel.forst@smoula.us>
2019-03-11 14:36:45 +0100

    Odstraneny soubory doc, xml ulozeno na

--- bin/rn-test.bat
+++
@@ -1,3 +0,0 @@
-doc-generator.bat Vzor_changes.xml x.xml _rn-test.doc RN.doc
-rem doc-generator.bat Vzor_changes.xml x.xml _str-test.doc STR.doc
-rem doc-generator.bat Vzor_changes.xml x.xml _mail-test.doc MAIL.doc
