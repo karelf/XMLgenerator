c7495f89bca3bb131110fefc41a0586c950a441c
Karel Foršt <karel.forst@smoula.us>
2019-10-11 13:12:26 +0200

    Funkcni generovnoi STR a RB

--- bin/run-gen-RN.bat
+++ bin/run-gen-RN.bat
@@ -1,1 +1,1 @@
-./doc-generator.bat Vzor_changes.xml final-xml.xml _rn-test.doc RN.doc
+.\doc-generator.bat Vzor_changes_All3rdDependencyCopy.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
