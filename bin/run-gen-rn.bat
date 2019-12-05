6edd9a22fb1e8369b8e0247df0111b713d5e7e6d
Karel Foršt <karel.forst@smoula.us>
2019-12-05 14:14:22 +0100

    Generovani RN a STR funkcni, availability other nefunguje nazev a reason

--- bin/run-gen-rn.bat
+++ bin/run-gen-rn.bat
@@ -1,1 +1,2 @@
-.\doc-generator.bat Vzor_changes_All3rdDependencyCopy.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
+rem .\doc-generator.bat Vzor_changes_All3rdDependencyCopy.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
+.\doc-generator.bat Vzor_changes.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
