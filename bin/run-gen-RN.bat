ca2b188328827604e7a139ea4920fabadb915048
Karel Foršt <karel.forst@smoula.us>
2020-01-03 09:01:48 +0100

    no message

--- bin/run-gen-RN.bat
+++
@@ -1,1 +0,0 @@
-.\doc-generator.bat Vzor_changes_All3rdDependencyCopy.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
--- bin/run-gen-rn.bat
+++
@@ -1,9 +0,0 @@
-rem .\doc-generator.bat Vzor_changes_All3rdDependencyCopy.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
-
-rem .\doc-generator.bat Vzor_changes_All3rdDependencyCopy.xml final-xml.xml _rn-test.doc docGener
-.\doc-generator.bat Vzor_changes.xml final-xml.xml _rn-test.doc docGenerator\ReleaseNotes.doc
-
-
-
-
-
